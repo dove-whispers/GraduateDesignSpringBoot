b5ee8901053b95b76a7813f0c3bc8c28c2e23d6f
dove-whispers <2209238480@qq.com>
2023-04-16 00:18:11 +0800

    4月15号查看报销单信息已拓展版本(差图片和comment在拓展区域内)

--- src/main/resources/static/js/admin/main-expense-report-detail.js
+++ src/main/resources/static/js/admin/main-expense-report-detail.js
@@ -24,18 +24,37 @@ $(function () {
         data.map(function (item, index) {
             html += '<tr>'
                 + '<th scope="row">' + (index + 1) + '</th>'
-                + '<td><input class="form-control detail_item" type="text" disabled value="'+item.item+'"></td>'
-                + '<td><input class="form-control detail_item" type="text" disabled value="'+item.time+'"></td>'
-                + '<td><input class="form-control detail_item" type="text" disabled value="'+item.type+'"></td>'
-                + '<td><input class="form-control detail_item" type="text" disabled value="'+item.code+'"></td>'
-                + '<td><input class="form-control detail_item" type="text" disabled value="'+item.num+'"></td>'
-                + '<td><input class="form-control detail_item" type="text" disabled value="'+item.amount+'"></td>'
+                + '<td><input class="form-control detail_item" type="text" disabled value="' + item.item + '"></td>'
+                + '<td><input class="form-control detail_item" type="text" disabled value="' + item.time + '"></td>'
+                + '<td><input class="form-control detail_item" type="text" disabled value="' + item.type + '"></td>'
+                + '<td><input class="form-control detail_item" type="text" disabled value="' + item.code + '"></td>'
+                + '<td><input class="form-control detail_item" type="text" disabled value="' + item.num + '"></td>'
+                + '<td><input class="form-control detail_item" type="text" disabled value="' + item.amount + '"></td>'
                 + '<td>'
-                + '<a class="btn btn-link pre-view hide" href="javascript:void(0)">预览</a>'
-                + '<small class="none-tip text-truncate">未选择任何文件</small>'
+                + '<a class="btn btn-xs btn-default d-cell" href="#collapse' + (index + 1) + '" data-toggle="collapse" title="更多" data-toggle="tooltip"><i class="mdi mdi-dots-horizontal"></i></a>'
+                + '</td>'
+                + '</tr>'
+                + '<tr>'
+                + '<td colspan="8" class="collapse" id="collapse' + (index + 1) + '">'
+                + '<div class="well m-b-0">'
+                + '笑谈渴饮匈奴血，壮志饥餐胡虏肉。'
+                + '</div>'
                 + '</td>'
                 + '</tr>'
         })
         body.html(html)
     }
+
+    body.on('click', 'a.d-cell', function () {
+        let ntd = $(this).parent().parent().next().children()
+        if (ntd.hasClass("d-table-cell")) {
+            ntd.removeClass("d-table-cell")
+        } else {
+            $('.collapse').each(function (index, element) {
+                $(element).removeClass("d-table-cell")
+                $(element).removeClass("in")
+            })
+            ntd.addClass("d-table-cell")
+        }
+    })
 })

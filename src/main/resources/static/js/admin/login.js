697f70c81558d724bd8c3cff260fcf435bbbcafa
dove-whispers <2209238480@qq.com>
2023-04-22 15:19:12 +0800

    4月22号再次完善。(包括badge和登陆页面重绘)

--- src/main/resources/static/js/admin/login.js
+++ src/main/resources/static/js/admin/login.js
@@ -1,18 +1,9 @@
 $(function () {
-    let loginCount = 0
+    $('#captcha').click()
     $('#submit').click(function () {
         let username = $('#username').val()
         let password = $('#password').val()
         let captchaCode = $('#j_captcha').val()
-        let needVerify = false
-        if (loginCount >= 3) {
-            if (!captchaCode) {
-                lightyear.notify('请输入验证码', 'danger', 2000, 'mdi mdi-emoticon-sad', 'top', 'center')
-                return
-            }
-            needVerify = true
-        }
-
         $.ajax({
             url: '/checkLogin',
             type: 'POST',
@@ -23,7 +14,7 @@ $(function () {
             data: JSON.stringify({
                 userName: username,
                 password,
-                needVerify,
+                needVerify: true,
                 verifyCodeActual: captchaCode
             }),
             success: function (data) {
@@ -31,12 +22,6 @@ $(function () {
                     lightyear.notify('登陆成功,欢迎' + data.username + "~", 'success', 2000, 'mdi mdi-emoticon-tongue', 'top', 'center', "/main")
                 } else {
                     lightyear.notify(data.errorMsg, 'danger', 2000, 'mdi mdi-emoticon-sad', 'top', 'center')
-                    loginCount++
-                    if (loginCount >= 3) {
-                        if (loginCount === 3) lightyear.notify('尝试次数过多,请输入验证码', 'warning', 2000, 'mdi mdi-emoticon-dead', 'top', 'center')
-                        $('#verifyPart').show()
-                        $('#captcha').click()
-                    }
                 }
             }
         })

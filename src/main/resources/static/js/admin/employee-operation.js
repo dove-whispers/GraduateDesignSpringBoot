29cb6ad887d264d0dfc181faf62c659e2b063699
dove-whispers <2209238480@qq.com>
2023-04-22 22:45:41 +0800

    4月22号修复员工查看详细信息的下拉不显示的bug

--- src/main/resources/static/js/admin/employee-operation.js
+++ src/main/resources/static/js/admin/employee-operation.js
@@ -23,6 +23,7 @@ $(function () {
             }
         }
     })
+
     $.ajax({
         url: queryActivePositionListUrl,
         type: 'POST',
@@ -32,96 +33,102 @@ $(function () {
         success: function (data) {
             if (data.success) {
                 activePositionList = data.data
+                process()
             }
         }
     })
+
     //非空则说明用户在查看或编辑
-    if (em_id) {
-        let employee = {}
-        let department_select = $('#department-select')
-        let position_select = $('#position-select')
-        $.ajax({
-            url: queryEmployeeUrl,
-            type: 'POST',
-            async: false,
-            cache: false,
-            dataType: 'json',
-            data: {
-                emId: em_id,
-            },
-            success: function (data) {
-                if (data.success) {
-                    employee = data.data
-                    $('#employee-name').val(employee.name)
-                    $('#login-name').val(employee.loginName)
-                    if (1 === employee.status) {
-                        $('#status-on').attr('checked', true)
-                        $('#status-off').removeAttr('checked')
+    function process() {
+        if (em_id) {
+            let employee = {}
+            let department_select = $('#department-select')
+            let position_select = $('#position-select')
+            $.ajax({
+                url: queryEmployeeUrl,
+                type: 'POST',
+                async: false,
+                cache: false,
+                dataType: 'json',
+                data: {
+                    emId: em_id,
+                },
+                success: function (data) {
+                    if (data.success) {
+                        employee = data.data
+                        $('#employee-name').val(employee.name)
+                        $('#login-name').val(employee.loginName)
+                        if (1 === employee.status) {
+                            $('#status-on').attr('checked', true)
+                            $('#status-off').removeAttr('checked')
+                        } else {
+                            $('#status-off').attr('checked', true)
+                            $('#status-on').removeAttr('checked')
+                        }
+                        if (is_edit) {
+                            activeDepartmentList.map(function (item, index) {
+                                let department_option = $('<option></option>')
+                                department_option.prop('value', item.depId)
+                                department_option.text(item.name)
+                                department_select.append(department_option)
+                            })
+                            department_select.selectpicker('refresh')
+                            department_select.selectpicker('val', employee.depId)
+                            activePositionList.map(function (item, index) {
+                                let position_option = $('<option></option>')
+                                position_option.prop('value', item.positionId)
+                                position_option.text(item.positionName)
+                                position_select.append(position_option)
+                            })
+                            position_select.selectpicker('refresh')
+                            position_select.selectpicker('val', employee.positionId)
+                        } else {
+                            activeDepartmentList.map(function (item, index) {
+                                if (employee.depId === item.depId) {
+                                    let department_option = $('<option></option>')
+                                    department_option.text(item.name)
+                                    department_select.append(department_option)
+                                    department_select.selectpicker('refresh')
+                                    department_select.selectpicker('render')
+                                }
+                            })
+                            activePositionList.map(function (item, index) {
+                                if (employee.positionId === item.positionId) {
+                                    let position_option = $('<option></option>')
+                                    position_option.text(item.positionName)
+                                    position_select.append(position_option)
+                                    position_select.selectpicker('refresh')
+                                    position_select.selectpicker('render')
+                                }
+                            })
+                        }
                     } else {
-                        $('#status-off').attr('checked', true)
-                        $('#status-on').removeAttr('checked')
+                        lightyear.notify('查询失败!', 'danger', 2000, 'mdi mdi-emoticon-sad', 'top', 'center')
                     }
-                } else {
-                    lightyear.notify('查询失败!', 'danger', 2000, 'mdi mdi-emoticon-sad', 'top', 'center')
                 }
-            }
-        })
-        if (is_edit) {
+            })
+        } else {
+            let department_target = $('#department-select')
             activeDepartmentList.map(function (item, index) {
                 let department_option = $('<option></option>')
                 department_option.prop('value', item.depId)
                 department_option.text(item.name)
-                department_select.append(department_option)
+                department_target.append(department_option)
             })
-            department_select.selectpicker('refresh')
-            department_select.selectpicker('val',employee.depId)
+            department_target.selectpicker('refresh')
+            department_target.selectpicker('render')
+            let position_target = $('#position-select')
             activePositionList.map(function (item, index) {
                 let position_option = $('<option></option>')
                 position_option.prop('value', item.positionId)
                 position_option.text(item.positionName)
-                position_select.append(position_option)
+                position_target.append(position_option)
             })
-            position_select.selectpicker('refresh')
-            position_select.selectpicker('val',employee.positionId)
-        } else {
-            activeDepartmentList.map(function (item, index) {
-                if (employee.depId === item.depId) {
-                    department_option.text(item.name)
-                    department_select.append(department_option)
-                    department_select.selectpicker('refresh')
-                    department_select.selectpicker('render')
-                }
-            })
-            activePositionList.map(function (item, index) {
-                if (employee.positionId === item.positionId) {
-                    position_option.text(item.positionName)
-                    position_select.append(position_option)
-                    position_select.selectpicker('refresh')
-                    position_select.selectpicker('render')
-                }
-            })
+            position_target.selectpicker('refresh')
+            position_target.selectpicker('render')
         }
-
-    } else {
-        let department_target = $('#department-select')
-        activeDepartmentList.map(function (item, index) {
-            let department_option = $('<option></option>')
-            department_option.prop('value', item.depId)
-            department_option.text(item.name)
-            department_target.append(department_option)
-        })
-        department_target.selectpicker('refresh')
-        department_target.selectpicker('render')
-        let position_target = $('#position-select')
-        activePositionList.map(function (item, index) {
-            let position_option = $('<option></option>')
-            position_option.prop('value', item.positionId)
-            position_option.text(item.positionName)
-            position_target.append(position_option)
-        })
-        position_target.selectpicker('refresh')
-        position_target.selectpicker('render')
     }
+
     $('#submit').click(function () {
         let name = $('#employee-name').val()
         let loginName = $('#login-name').val()

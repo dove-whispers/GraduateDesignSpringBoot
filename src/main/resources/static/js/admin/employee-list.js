a4aed706e7926f38d3d92c5bc86b5fae91594af9
dove-whispers <2209238480@qq.com>
2023-04-22 14:57:15 +0800

    4月22号改登陆页面前

--- src/main/resources/static/js/admin/employee-list.js
+++ src/main/resources/static/js/admin/employee-list.js
@@ -34,6 +34,26 @@ $(function () {
 
     getList(request_condition)
 
+    $.ajax({
+        url: '/expenseReport/getViewList',
+        type: 'POST',
+        async: false,
+        cache: false,
+        dataType: 'json',
+        contentType: 'application/json;charset=utf-8',
+        data: JSON.stringify({}),
+        success: function (data) {
+            if (data.success) {
+                let len = data.data.records.length
+                if (0 !== len) {
+                    $('.badge').text(len)
+                }
+            } else {
+                lightyear.notify(data.errMsg, 'danger', 2000, 'mdi mdi-emoticon-sad', 'top', 'center')
+            }
+        }
+    })
+
     //获取下拉列表数据
     $.ajax({
         url: queryActivePositionListUrl,
--- src/main/resources/static/js/admin/expense-report-add.js
+++ src/main/resources/static/js/admin/expense-report-add.js
@@ -1,4 +1,24 @@
 $(function () {
+    $.ajax({
+        url: '/expenseReport/getViewList',
+        type: 'POST',
+        async: false,
+        cache: false,
+        dataType: 'json',
+        contentType: 'application/json;charset=utf-8',
+        data: JSON.stringify({}),
+        success: function (data) {
+            if (data.success) {
+                let len = data.data.records.length
+                if (0 !== len) {
+                    $('.badge').text(len)
+                }
+            } else {
+                lightyear.notify(data.errMsg, 'danger', 2000, 'mdi mdi-emoticon-sad', 'top', 'center')
+            }
+        }
+    })
+
     $('#detail-body').on('change', '.detail_image', async function (e) {
         let fileStr = await changeFileIntoBase64($(this)[0].files[0])
         let ttd = $(this).closest('td').next()
